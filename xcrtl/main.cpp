e943d1f2bac1a85461c0f813a74d05f2cb6ad420
Gordon Childs <gordon.childs@gmail.com>
2014-01-25 09:56:42 +1100

    another ill-fated project

---
+++ xcrtl/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  xcrtl
+//
+//  Created by Childs, Gordon on 25/01/2014.
+//  Copyright (c) 2014 async. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
